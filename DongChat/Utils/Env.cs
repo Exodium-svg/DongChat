b35735717d25adb1d335238bb21a11a90ea76c8e
Exodium <Nem56@NEMO-WINDOWS>
2025-02-11 18:36:25 +0100

    Add project files.

---
+++ DongChat/Utils/Env.cs
@@ -0,0 +1,181 @@
+﻿using System.Collections.Concurrent;
+using System.Runtime.InteropServices;
+
+namespace Common.Utils
+{
+    public enum CVarType : byte
+    {
+        Byte = 1,
+        Char = 2,
+        Int16 = 3,
+        UInt16 = 4,
+        Int32 = 5,
+        UInt32 = 6,
+        Int64 = 7,
+        UInt64 = 8,
+        String = 9
+    }
+    public readonly struct CVar
+    {
+        public readonly CVarType cType;
+        public readonly object cData;
+
+        public CVar(CVarType cVarType, object cVar)
+        {
+            this.cType = cVarType;
+            this.cData = cVar;
+        }
+    }
+    public sealed class Env
+    {
+        readonly ConcurrentDictionary<string, CVar> _cVars = new();
+        private void LoadVariables(byte[] cVarData)
+        {
+            using MemoryStream ms = new MemoryStream(cVarData);
+
+            string signature = ms.ReadString();
+            int count = ms.Read<int>();
+
+            for (int i = 0; i < count; i++)
+            {
+                CVarType cVarType = (CVarType)ms.ReadByte();
+                string cVarName = ms.ReadString();
+
+                object cVar = cVarType switch
+                {
+                    CVarType.Byte => (byte)ms.ReadByte(),
+                    CVarType.Char => (char)ms.Read<ushort>(),  // Use UInt16 since .NET chars are 2 bytes (UTF-16)
+                    CVarType.Int16 => ms.Read<short>(),
+                    CVarType.UInt16 => ms.Read<ushort>(),
+                    CVarType.Int32 => ms.Read<int>(),
+                    CVarType.UInt32 => ms.Read<uint>(),
+                    CVarType.Int64 => ms.Read<long>(),
+                    CVarType.UInt64 => ms.Read<ulong>(),
+                    _ => throw new InvalidDataException($"Unknown CVarType: {cVarType}")
+                };
+
+                _cVars[cVarName] = new CVar(cVarType, cVar);
+            }
+        }
+
+        public Env(ReadOnlyMemory<byte> cVarData)
+        {
+            if(!MemoryMarshal.TryGetArray(cVarData, out ArraySegment<byte> segment))
+                segment = new ArraySegment<byte>(cVarData.ToArray());
+            
+            LoadVariables(segment.Array!);
+        }
+
+        public Env(string path) => LoadVariables(File.ReadAllBytes(path));
+        public void SetVar(string key, CVar cVar) => _cVars[key] = cVar;
+        public string GetString(string key, string fallback)
+        {
+            if(_cVars.TryGetValue(key, out CVar cVar))
+                return (string)cVar.cData;
+
+            return fallback;
+        }
+        public T Get<T>(string key, T fallback) where T : struct
+        {
+            if (!_cVars.TryGetValue(key, out CVar cVar))
+                return fallback;
+
+            switch(cVar.cType)
+            {
+                case CVarType.Byte:
+                    if (typeof(T) != typeof(byte))
+                        throw new NotSupportedException($"{key} is not of type {typeof(T)}, it is of type {typeof(byte)}");
+                    break;
+
+                case CVarType.Char:
+                    if (typeof(T) != typeof(char))
+                        throw new NotSupportedException($"{key} is not of type {typeof(T)}, it is of type {typeof(char)}");
+                    break;
+
+                case CVarType.Int16:
+                    if (typeof(T) != typeof(short))
+                        throw new NotSupportedException($"{key} is not of type {typeof(T)}, it is of type {typeof(short)}");
+                    break;
+
+                case CVarType.UInt16:
+                    if (typeof(T) != typeof(ushort))
+                        throw new NotSupportedException($"{key} is not of type {typeof(T)}, it is of type {typeof(ushort)}");
+                    break;
+
+                case CVarType.Int32:
+                    if (typeof(T) != typeof(int))
+                        throw new NotSupportedException($"{key} is not of type {typeof(T)}, it is of type {typeof(int)}");
+                    break;
+
+                case CVarType.UInt32:
+                    if (typeof(T) != typeof(uint))
+                        throw new NotSupportedException($"{key} is not of type {typeof(T)}, it is of type {typeof(uint)}");
+                    break;
+
+                case CVarType.Int64:
+                    if (typeof(T) != typeof(long))
+                        throw new NotSupportedException($"{key} is not of type {typeof(T)}, it is of type {typeof(long)}");
+                    break;
+
+                case CVarType.UInt64:
+                    if (typeof(T) != typeof(ulong))
+                        throw new NotSupportedException($"{key} is not of type {typeof(T)}, it is of type {typeof(ulong)}");
+                    break;
+
+                default:
+                    throw new NotSupportedException($"Unsupported CVarType: {cVar.cType}");
+            }
+
+            return (T)cVar.cData;
+        }
+        public void Save(string output)
+        {
+            using FileStream fs = File.OpenWrite(output);
+
+            fs.WriteString("EnvironmentVar");
+            fs.Write(_cVars.Count);
+
+            foreach(KeyValuePair<string, CVar> cVarPair in  _cVars)
+            {
+                CVar cVar = cVarPair.Value;
+                fs.WriteByte((byte)cVar.cType);
+                fs.WriteString(cVarPair.Key);
+                
+                switch(cVar.cType)
+                {
+                    case CVarType.Byte:
+                        fs.Write((byte)cVar.cData);
+                        break;
+                    case CVarType.Char:
+                        fs.Write((ushort)(char)cVar.cData); // Cast `char` to `ushort` (since .NET `char` is 2 bytes)
+                        break;
+                    case CVarType.Int16:
+                        fs.Write((short)cVar.cData);
+                        break;
+                    case CVarType.UInt16:
+                        fs.Write((ushort)cVar.cData);
+                        break;
+                    case CVarType.Int32:
+                        fs.Write((int)cVar.cData);
+                        break;
+                    case CVarType.UInt32:
+                        fs.Write((uint)cVar.cData);
+                        break;
+                    case CVarType.Int64:
+                        fs.Write((long)cVar.cData);
+                        break;
+                    case CVarType.UInt64:
+                        fs.Write((ulong)cVar.cData);
+                        break;
+                    case CVarType.String:
+                        fs.WriteString((string)cVar.cData);
+                    break;
+                    default:
+                        throw new InvalidDataException($"Unsupported CVarType: {cVar.cType}");
+                }
+            }
+
+            fs.Close();
+        }
+    }
+}
---
+++ DongChat/Utils/StreamExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Common.Utils
+{
+    public static class StreamExtensions
+    {
+        public static T Read<T>(this Stream stream) where T : struct
+        {
+            Span<byte> buffer = stackalloc byte[Marshal.SizeOf<T>()];
+            stream.ReadExactly(buffer);
+
+            return MemoryMarshal.Read<T>(buffer);
+        }
+
+        public static string ReadString(this Stream stream)
+        {
+            int strLen = stream.Read<int>();
+
+            Span<byte> strBuff = strLen > 1024 ? new byte[strLen] : stackalloc byte[strLen];
+            stream.ReadExactly(strBuff);
+
+            return Encoding.UTF8.GetString(strBuff);
+        }
+
+        public static void Write<T>(this Stream stream, T value) where T : struct
+        {
+            Span<byte> buffer = stackalloc byte[Marshal.SizeOf<T>()];
+            MemoryMarshal.Write(buffer, value);
+
+            stream.Write(buffer);
+        }
+
+        public static void WriteString(this Stream stream, string value)
+        {
+            Span<byte> strBytes = Encoding.UTF8.GetBytes(value);
+            stream.Write<int>(strBytes.Length);
+            stream.Write(strBytes);
+        }
+    }
+}
